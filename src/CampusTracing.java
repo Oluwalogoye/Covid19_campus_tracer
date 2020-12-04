7bc0ea3e2b9f7823bc34cf8f1f8c8cf03978c163
fritzpeace <fritz@pazmail.ch>
2020-12-03 22:51:25 -0500

    Update displayStatistics() in CampusTracing.java
    added displayStatistics() code
    created helper class getRiskLevel to make code tidy

--- src/CampusTracing.java
+++ src/CampusTracing.java
@@ -59,9 +59,87 @@ public class CampusTracing {
     }
 
     /**
+     * @param percentInfected
+     * helper method that returns risk level String to make code tidy
+     */
+    private String getRiskLevel(int percentInfected)
+    {
+        if(percentInfected <= 5)
+        {
+            return "Low";
+        }
+        else if(percentInfected <= 10)
+        {
+            return "Low Moderate";
+        }
+        else if(percentInfected <= 20)
+        {
+            return "Moderate";
+        }
+        else
+        {
+            return "High";
+        }
+    }
+
+    /**
      * @param p
      */
-    public void displayStatistics(ArrayList<Student> p) {
+    public void displayStatistics(ArrayList<Student> p) 
+    {
+        // Need to figure out what data structure to make lastChecked.
+        // Maybe an ArrayList of booleans for each day since semester started
+        if(!lastChecked(day))
+        {
+                testStudents(p);
+        }
+
+        // Statistics data holder
+        ArrayList<Student> infected;
+        ArrayList<Student> quarantined;
+        ArrayList<Student> immune;
+        int daysInQuarantine;
+        int firstDayS, lastDayS;
         
+        // Populate statistics
+        for(Student s : p)
+        {
+            if(s.isInfected())
+            {
+                infected.add(s);
+            }
+            if(s.isQuarantined())
+            {
+                quarantined.add(s);
+                daysInQuarantine += s.getDaysInQuarantine();
+                // Need to make sure the cases are right
+                if(s.getDaysInQuarantine() == 1)
+                {
+                    
+                    firstDayS++;
+                }
+                else if(s.getDaysInQuarantine() == 14)
+                {
+                    lastDayS++;
+                }
+            }
+            if(s.isImmune())
+            {
+                immune.add(s);
+            }
+        }
+
+        // Process Statistics
+        ArrayList<String> statistics;                                   // Hold all the strings to be printed
+        int percentInfected = (infected.size() / p.size()) * 100;       // Percent of Infected People
+        String riskLevel = getRiskLevel(percentInfected);               // Risk Level
+        int avgQuarantine = daysInQuarantine / quarantined.size();      // Mean Days Spent in Quarantine
+        // graph days spent in quarantine. calculate variance/std dev.
+
+        statistics.add("There are " + infected.size() + " infected studies.");
+        statistics.add("This makes up for " + percentInfected + "% of the Student Body.");
+        statistics.add("There are " + quarantined.size() + "Students in quarantine, which have completed an average of " + avgQuarantine + " days already spent.");
+        statistics.add( lastDayS + " students are doing their last day of quarantine, while " + firstDayS + " have been quarantined today");
+        statistics.add("The campus is at a "+ riskLevel + " risk level.");
     }
 }
