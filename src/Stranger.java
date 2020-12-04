c81cfad42bf10c62783170adbb76508028bbe57c
Oluwalogoye <48224211+Oluwalogoye@users.noreply.github.com>
2020-12-04 17:54:23 -0500

    Update Stranger.java

--- src/Stranger.java
+++ src/Stranger.java
@@ -1,22 +1,11 @@
 
 public class Stranger extends Person{
 	
-	// data members
-	private float riskFactor;
 
 	// constructors
-	public Stranger(boolean isInfected, boolean isQuarantined, boolean isImmune, int daysInQuarantine, String name, int age, float riskFactor) {
-		super(isInfected, isQuarantined, isImmune, daysInQuarantine,  name, age);
-		this.riskFactor = riskFactor;
-	}
-
-	// methods
-	public float getRiskFactor() {
-		return riskFactor;
-	}
-
-	public void setRiskFactor(float riskFactor) {
-		this.riskFactor = riskFactor;
+	public Stranger(boolean isInfected, boolean isQuarantined, boolean isImmune, int daysInQuarantine,
+			float infectionProb, String name, int age) {
+		super(isInfected, isQuarantined, isImmune, daysInQuarantine, infectionProb, name, age);
 	}
 	
 	
