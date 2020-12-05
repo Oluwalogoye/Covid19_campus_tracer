4795ca3dde280ea5ce0fd8ed834f5aead627e234
Oluwalogoye <48224211+Oluwalogoye@users.noreply.github.com>
2020-12-04 21:56:51 -0500

    Update Classroom.java

--- src/Classroom.java
+++ src/Classroom.java
@@ -4,13 +4,17 @@ import java.util.ArrayList;
 public class Classroom {
 	
 	// data members
+	private String name;
 	private int num;
 	private ArrayList<Student> peers;
+	private Teacher teacher;
 	
 	// constructors
-	public Classroom(int num, ArrayList<Student> peers) {
+	public Classroom(String name, int num, ArrayList<Student> peers, Teacher teacher) {
+		this.name = name;
 		this.num = num;
 		this.peers = peers;
+		this.teacher = teacher;
 	}
 	
 	// methods
@@ -25,5 +29,20 @@ public class Classroom {
 	}
 	public void setPeers(ArrayList<Student> peers) {
 		this.peers = peers;
+	}
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Teacher getTeacher() {
+		return teacher;
+	}
+
+	public void setTeacher(Teacher teacher) {
+		this.teacher = teacher;
 	}
 }
